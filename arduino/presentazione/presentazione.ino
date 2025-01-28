2b3d77f3343bb587bbbf330abcf3e1fb218df210
lory-120 <lorenzo.turri12@gmail.com>
2025-01-28 15:08:12 +0100

    aggiunto lo sketch per presentazione e aggiornamenti alle librerie

---
+++ arduino/presentazione/presentazione.ino
@@ -0,0 +1,24 @@
+//definiamo i pin da utilizzare per l'I/O
+const int inputPin = 7; //il pin di ingresso
+const int outputPin = 8; //il pin di uscita
+
+void setup() { //eseguita solo una volta, a meno che non sia richiamata nel loop()
+  Serial.begin(9600); //inizializza la comunicazione seriale
+  pinMode(inputPin, INPUT); //imposta il pin di input
+  pinMode(outputPin, OUTPUT); //imposta il pin di output
+}
+
+void loop() { //eseguita all'infinito, fino a quando non si spegne la scheda o c'è un errore fatale
+  bool pinState = digitalRead(inputPin) //leggi lo stato dal pin di input
+  if(pinState == HIGH) { //se lo stato è HIGH:
+    digitalWrite(outputPin, HIGH); //imposta lo stato del pin di output a HIGH (= acceso)
+    Serial.print("Pulsante nel GPIO ");
+    Serial.print(inputPin);
+    Serial.println(" premuto."); //stampa una riga di messaggio nel Serial Monitor
+  } else { //se invece è a LOW:
+    digitalWrite(outputPin, LOW); //imposta lo stato del pin di output a LOW (= spento)
+    Serial.print("Pulsante nel GPIO ");
+    Serial.print(inputPin);
+    Serial.println(" non premuto."); //stampa una riga di messaggio nel Serial Monitor
+  }
+}
